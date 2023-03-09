923d1b45b9c2f07ff02fd8bbed880d1439f76aae
rafael.ferreira <rafael.ferreira1@aluno.ifsp.edu.br>
2023-03-09 11:58:12 -0300

    first commit

---
+++ open.js
@@ -0,0 +1,95 @@
+const btn1 = document.getElementById("aspasia");
+const btn2 = document.getElementById("cleobulina");
+const btn3 = document.getElementById("damaris");
+const btn4 = document.getElementById("diotimademantineia");
+const btn5 = document.getElementById("djamila_ribeiro");
+const btn6 = document.getElementById("duoda");
+const btn7 = document.getElementById("gertrude");
+const btn8 = document.getElementById("hannaharendt");
+const btn9 = document.getElementById("judithbutler");
+const btn10 = document.getElementById("lelia_gonzales");
+const btn11 = document.getElementById("mariazambranno");
+const btn12 = document.getElementById("mariedegourna");
+const btn13 = document.getElementById("marywollstonecraft");
+const btn14 = document.getElementById("nancy_fraser");
+const btn15 = document.getElementById("olympe_gouges");
+const btn16 = document.getElementById("philippa_foot");
+const btn17 = document.getElementById("rosaluxemburgo");
+const btn18 = document.getElementById("sarahkofman");
+const btn19 = document.getElementById("simonedebeauvoir");
+const btn20 = document.getElementById("simoneweil");
+const btn21 = document.getElementById("lady_murasaki");
+const btn22 = document.getElementById("susanhack");
+
+
+btn1.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Aspásia de Mileto.pdf");
+});
+btn2.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Cleobulina.pdf");
+});
+
+btn3.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Damaris Masham.pdf");
+});
+btn4.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Diotima de Mantineia.pdf");
+});
+btn5.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Djamila Ribeiro.pdf");
+});
+btn6.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Duoda.pdf");
+});
+
+btn7.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Gertrude Anscombe.pdf");
+});
+
+btn8.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Hannah Arendt.pdf");
+});
+
+btn9.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Judith Butler.pdf");
+});
+
+btn10.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Lélia Gonzales.pdf");
+});
+btn11.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Maria de Gournay.pdf");
+});
+btn12.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Mary Wollstonecraft.pdf");
+});
+btn13.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/María Zambrano.pdf");
+});
+btn14.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Nancy Fraser.pdf");
+});
+btn15.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Olympe de Gouges.pdf");
+});
+btn16.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Philippa Foot.pdf");
+});
+btn17.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Rosa Luxemburgo.pdf");
+});
+btn18.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Sarah Kofman.pdf");
+});
+btn19.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Simone de Beauvoir.pdf");
+});
+btn20.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Simone Weil.pdf");
+});
+btn21.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Lady Murasaki.pdf");
+});
+btn22.addEventListener("click", function() {
+  window.open("Arquivos/Pdfs/Susan Haack.pdf");
+});
